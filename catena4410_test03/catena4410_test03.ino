02323d19c5bb892c13cbf00c60a532fa7ab7e493
Terry Moore <tmm@mcci.com>
2016-10-24 22:02:38 -0400

    Copy from catena4410_test02.ino

---
+++ catena4410_test03/catena4410_test03.ino
@@ -0,0 +1,301 @@
+/* catena4410_test02.ino	Sat Oct 15 2016 23:08:27 tmm */
+
+/*
+
+Module:  catena4410_test02.ino
+
+Function:
+	Test program #2 for the Catena 4410.
+
+Version:
+	V0.1.0	Sat Oct 15 2016 23:08:27 tmm	Edit level 1
+
+Copyright notice:
+	This file copyright (C) 2016 by
+
+		MCCI Corporation
+		3520 Krums Corners Road
+		Ithaca, NY  14850
+
+	An unpublished work.  All rights reserved.
+	
+	This file is proprietary information, and may not be disclosed or
+	copied without the prior permission of MCCI Corporation.
+ 
+Author:
+	Terry Moore, MCCI Corporation	October 2016
+
+Revision history:
+   1.00a  Sat Oct 15 2016 23:08:27  tmm
+	Module created.
+
+*/
+
+#include <Catena4410.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+#include <Adafruit_BME280.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SHT1x.h>
+
+/****************************************************************************\
+|
+|		Manifest constants & typedefs.
+|
+|	This is strictly for private types and constants which will not 
+|	be exported.
+|
+\****************************************************************************/
+
+// manifests.
+//#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA (1027.087) // 3170 Perry City Road, 2016-10-04 22:57
+
+// forwards
+static void configureLuxSensor(void);
+static void displayLuxSensorDetails(void);
+static bool displayTempSensorDetails(void);
+
+/****************************************************************************\
+|
+|	Read-only data.
+|
+|	If program is to be ROM-able, these must all be tagged read-only 
+|	using the ROM storage class; they may be global.
+|
+\****************************************************************************/
+
+
+/****************************************************************************\
+|
+|	VARIABLES:
+|
+|	If program is to be ROM-able, these must be initialized
+|	using the BSS keyword.  (This allows for compilers that require
+|	every variable to have an initializer.)  Note that only those 
+|	variables owned by this module should be declared here, using the BSS
+|	keyword; this allows for linkers that dislike multiple declarations
+|	of objects.
+|
+\****************************************************************************/
+
+
+
+// globals
+Catena4410 gCatena4410;
+
+//   The temperature/humidity sensor
+Adafruit_BME280 bme; // The default initalizer creates an I2C connection
+bool fBme;
+
+//   The LUX sensor
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+bool fTsl;
+
+//   The submersible temperature sensor
+OneWire oneWire(Catena4410::PIN_ONE_WIRE);
+DallasTemperature sensor_WaterTemp(&oneWire);
+bool fWaterTemp;
+
+//  The SHT10 soil sensor
+SHT1x sensor_Soil(Catena4410::PIN_SHT10_DATA, Catena4410::PIN_SHT10_CLK);
+bool fSoilSensor;
+
+void setup() 
+{
+    Catena4410::UniqueID_buffer_t CpuID;
+
+    while (!Serial); // wait for Serial to be initialized
+    Serial.begin(115200);
+
+    gCatena4410.SafePrintf("Basic Catena 4410 test\n");
+    gCatena4410.GetUniqueID(CpuID);
+
+    gCatena4410.SafePrintf("CPU Unique ID: ");
+    for (unsigned i = 0; i < sizeof(CpuID); ++i)
+    {
+      gCatena4410.SafePrintf("%s%02x", i == 0 ? "" : "-", CpuID[i]);
+    }
+    gCatena4410.SafePrintf("\n");
+
+    /* display info about the NVM configuration */
+    gCatena4410.SafePrintf("NVMCTRL register: %#010x\n", *(volatile uint32_t *) (NVMCTRL_USER));
+
+    /* find the platform */
+    Catena4410::EUI64_buffer_t SysEUI;
+
+    const CATENA_PLATFORM * const pPlatform = gCatena4410.GetPlatformForID(CpuID, SysEUI);
+    if (pPlatform)
+    {
+      gCatena4410.SafePrintf("EUI64: ");
+      for (unsigned i = 0; i < sizeof(SysEUI); ++i)
+      {
+        gCatena4410.SafePrintf("%s%02x", i == 0 ? "" : "-", SysEUI[i]);
+      }
+      gCatena4410.SafePrintf("\n");
+      gCatena4410.SafePrintf("Flags:  %#010x\n", pPlatform->Flags);
+    }
+
+    /* initialize the lux sensor */
+    if (! tsl.begin())
+    {
+      gCatena4410.SafePrintf("No TSL2561 detected: check wiring\n");
+      fTsl = false;
+    }
+    else
+    {
+      fTsl = true;
+      displayLuxSensorDetails();
+      configureLuxSensor();
+    }
+
+    /* initialize the BME280 */
+    if (! bme.begin())
+    {
+      gCatena4410.SafePrintf("No BME280 found: check wiring\n");
+      fBme = false;
+    }
+    else
+    {
+      fBme = true;
+    }
+
+    /* initialize the pond sensor */
+    sensor_WaterTemp.begin();
+
+     if (! displayTempSensorDetails())
+    {
+      gCatena4410.SafePrintf("water temperature not found: is it connected?\n");
+      fWaterTemp = false;
+    }
+    else
+    {
+      fWaterTemp = true;
+    }
+
+    /* initalize the soil sensor... not yet.*/
+    /* sensor_Soil.begin() */
+    fSoilSensor = true;
+}
+
+void loop() 
+{
+  if (fBme)
+  {
+    Serial.print("Vbat = "); Serial.print(gCatena4410.ReadVbat()); Serial.println(" V");
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+  }
+  else
+  {
+    gCatena4410.SafePrintf("No BME280 sensor\n");
+  }
+  if (fTsl)
+  {
+    /* Get a new sensor event */ 
+    sensors_event_t event;
+    tsl.getEvent(&event);
+   
+    /* Display the results (light is measured in lux) */
+    if (event.light)
+    {
+      Serial.print(event.light); Serial.println(" lux");
+    }
+    else
+    {
+      /* If event.light = 0 lux the sensor is probably saturated
+         and no reliable data could be generated! */
+      Serial.println("Sensor overload");
+    }
+  }
+  else
+  {
+    gCatena4410.SafePrintf("No Lux sensor\n");
+  }
+
+  if (fWaterTemp)
+  {
+    sensor_WaterTemp.requestTemperatures();
+    float waterTempC = sensor_WaterTemp.getTempCByIndex(0);
+    Serial.print("Water temperature: "); Serial.print(waterTempC); Serial.println(" C");
+  }
+  else
+  {
+    gCatena4410.SafePrintf("No water temperature\n");
+  }
+
+  if (fSoilSensor)
+  {
+    /* display temp and RH. library doesn't tell whether sensor is disconnected but gives us huge values instead */
+    Serial.print("Soil temperature: "); Serial.print(sensor_Soil.readTemperatureC()); Serial.println(" C");
+    Serial.print("Soil humidity:    "); Serial.print(sensor_Soil.readHumidity()); Serial.println(" %");
+  }
+  delay(2000);
+}
+
+/* functions */
+static void displayLuxSensorDetails(void)
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+static void configureLuxSensor(void)
+{
+  /* You can also manually set the gain or enable auto-gain support */
+  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
+  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
+  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  
+  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+
+  /* Update these values depending on what you've set above! */  
+  Serial.println("------------------------------------");
+  Serial.print  ("Gain:         "); Serial.println("Auto");
+  Serial.print  ("Timing:       "); Serial.println("13 ms");
+  Serial.println("------------------------------------");
+}
+
+
+static bool displayTempSensorDetails(void)
+{
+  const unsigned nDevices = sensor_WaterTemp.getDeviceCount();
+  if (nDevices == 0)
+    return false;
+
+  gCatena4410.SafePrintf("found %u devices\n", nDevices);
+  for (unsigned iDevice = 0; iDevice < nDevices; ++iDevice)
+  {
+    // print interesting info
+  }
+  return true;
+}
