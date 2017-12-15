db2202521ace91d247c83fcc91198e8a7064e813
Terry Moore <tmm@mcci.com>
2017-12-15 14:30:18 -0500

    Adapt for new architecture and make sketch arch independent

--- catena4450m101_sensor/ThisCatena.h
+++ catena4450m101_sensor/ThisCatena.h
@@ -1,14 +1,9 @@
+#include <Catena.h>
+using ThisCatena = McciCatena::Catena;
+
 #ifdef ARDUINO_ARCH_SAMD
-# include <Catena4450.h>
 # include <CatenaRTC.h>
-  using ThisCatena = McciCatena::Catena4450;
 # include <delay.h>
-#else
-# ifdef ARDUINO_ARCH_STM32
-# include <Catena4551.h>
+#elif defined(ARDUINO_ARCH_STM32)
 # include <CatenaStm32L0Rtc.h>
- using ThisCatena = McciCatena::Catena4551;
-# else
-#   error Architecture not supported
-# endif
 #endif
