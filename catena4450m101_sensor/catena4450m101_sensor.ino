6d7a23763cb6089a28f3b8ee38526bcf42bbd24b
Terry Moore <tmm@mcci.com>
2019-04-13 13:33:13 -0400

    Merge the app command into the main script

--- catena4450m101_sensor/catena4450m101_sensor.ino
+++ catena4450m101_sensor/catena4450m101_sensor.ino
@@ -867,4 +867,45 @@ void setTxCycleTime(
 // avoid the problem.
 //
 
-// #include "application_commands.cpp"
+#if ! defined(CATENA_ARDUINO_PLATFORM_VERSION)
+# error MCCI Catena Arduino Platform is out of date. Check CATENA_ARDUINO_PLATFORM_VERSION
+#elif (CATENA_ARDUINO_PLATFORM_VERSION < CATENA_ARDUINO_PLATFORM_VERSION_CALC(0, 14, 0, 50))
+# error MCCI Catena Arduino Platform is out of date. Check CATENA_ARDUINO_PLATFORM_VERSION
+#endif
+
+/* process "application tx-period" -- without args, display, with an arg set the value */
+// argv[0] is "tx-period"
+// argv[1] if present is the new value
+cCommandStream::CommandStatus
+setTransmitPeriod(
+	cCommandStream *pThis,
+	void *pContext,
+	int argc, 
+	char **argv
+	)
+	{
+        if (argc > 2)
+                return cCommandStream::CommandStatus::kInvalidParameter;
+
+        if (argc < 2)
+                {
+                pThis->printf("%u s\n", (unsigned) gTxCycle);
+                return cCommandStream::CommandStatus::kSuccess;
+                }
+        else
+                {
+                // convert argv[1] to unsigned
+                cCommandStream::CommandStatus status;
+                uint32_t newTxPeriod;
+
+                // get arg 1 as tx period; default is irrelevant because
+                // we know that we have argv[1] from above.
+                status = cCommandStream::getuint32(argc, argv, 1, /* radix */ 0, newTxPeriod, 0);
+
+                if (status != cCommandStream::CommandStatus::kSuccess)
+                        return status;
+
+                gTxCycle = newTxPeriod;
+                return status;
+                }
+        }
